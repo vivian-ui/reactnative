767de857d6f347f5c5889ae7849eb7c9610b2921
Vivian <66747981+vivian-ui@users.noreply.github.com>
2023-03-26 17:06:30 -0300

    Anúncios para venda de produtos

---
+++ 08app/styles.js
@@ -0,0 +1,43 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+   container:{
+    flex: 1
+  },
+  box1:{
+    backgroundColor: 'gray',
+    height: 150,
+    width: 100,
+  },
+  box2:{
+    backgroundColor: 'green',
+    height: 150,
+    width: 100,
+  },
+  box3:{
+    backgroundColor: 'yellow',
+    height: 150,
+    width: 100,
+  },
+  box4:{
+    backgroundColor: 'blue',
+    height: 150,
+    width: 100,
+  },
+  titulo: {
+    fontSize: 18,
+    alignSelf: 'center',
+    color: 'orange',
+    marginTop: 15,
+  },
+  image: {
+    width: 90, 
+    height: 80, 
+    marginTop: 10, 
+        alignSelf: 'center',
+   
+  },
+})
+
+
+  export {styles};
