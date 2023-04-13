cf24ffca4a7bef2ae13ad6a8381c923863e18d83
Laryssa Lisboa <100734683+laryssalisboa@users.noreply.github.com>
2023-04-12 21:12:53 -0300

    Meu Perfil

--- 01app/App.js
+++ 01app/App.js
@@ -1,35 +1,39 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
-import Constants from 'expo-constants';
 
-function App(){
-  return(
-    <View>
-      <View style={{alignItems: 'center', marginTop: 20}}>
-        <Text style={{fontSize: 20, color: 'blue'}}>Meu Perfil</Text>
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <View style={{alignItems: 'center', marginTop: 20}}>
+          <Text style={{fontSize: 20, color: 'blue'}}>Meu Perfil</Text>
 
-        <Image 
-          source={{uri: 'https://avatars.githubusercontent.com/u/66747981?v=4'}}
-          style={{width: 150, height: 150, marginTop: 10}}
-        />
+          <Image 
+            source={{uri: 'https://avatars.githubusercontent.com/u/66747981?v=4'}}
+            style={{width: 150, height: 150, marginTop: 10}}
+          />
+        </View>
+
+        <Text style={{fontWeight: 'bold', marginTop: 20}}>Dados pessoais</Text>
+        <Text>Vivian da Cunha, 47 anos, Solteira, Fatecana</Text> 
+
+        <Text style={{fontWeight: 'bold', marginTop: 20}}>Dados pessoais</Text>
+        <Text>Vivian da Cunha, 47 anos, Solteira, Fatecana</Text>
+
+        <Text style={{fontWeight: 'bold', marginTop: 10}}>Formação</Text>
+        <Text>FATEC BS - SI</Text>
+        <Text>Ciências da Computação</Text>
+
+        <Text style={{fontWeight: 'bold', marginTop: 10}}>Experiência</Text>
+        <Text>Estagiaria</Text>
+        <Text>Tecnica de informatica</Text>
+
+        <Text style={{fontWeight: 'bold', marginTop: 10}}>Projetos</Text>
+        <Text>Salesforce</Text>
+        <Text>Suporte</Text>
       </View>
-
-      <Text style={{fontWeight: 'bold', marginTop: 20}}>Dados pessoais</Text>
-      <Text>Vivian da Cunha, 47 anos, Solteira, Fatecana</Text>
-
-      <Text style={{fontWeight: 'bold', marginTop: 10}}>Formação</Text>
-      <Text>FATEC BS - SI</Text>
-      <Text>Ciências da Computação</Text>
-
-      <Text style={{fontWeight: 'bold', marginTop: 10}}>Experiência</Text>
-      <Text>Estagiaria</Text>
-      <Text>Tecnica de informatica</Text>
-
-      <Text style={{fontWeight: 'bold', marginTop: 10}}>Projetos</Text>
-      <Text>Salesforce</Text>
-      <Text>Suporte</Text>
-    </View>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
